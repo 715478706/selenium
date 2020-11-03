9c80858b7a8452119dce787735f2fbe16cee7ea6
blithe <qq@blithe.com>
2020-11-03 14:55:05 +0800

    重新修改了index.js

--- target/surefire-reports/index.js
+++ target/surefire-reports/index.js
@@ -1,3 +1,4 @@
+//因为默认的testng生成的报告有一个google脚本获取不到，这里通过node删除强求，这里记录一下
 const fs = require('fs');
 const cheerio = require('cheerio')
 
@@ -35,7 +36,15 @@ var writeFile = require('fs-writefile-promise');
 
 async function rw(path){
 	let r = await readFile('index.html')//r就是rw().next(data)进来的data
-	let w = await writeFile('index.html',r.toString(),'utf8')
+    // 加载HTML字符串
+    const $ = cheerio.load(r.toString())
+     // 设置Text
+    $('script').each(function(){
+        if ($(this).attr('src') =='https://www.google.com/jsapi'){
+            $(this).remove()
+        }
+    })
+	let w = await writeFile('index.html',$.html(),'utf8')
 	return w
 }
 rw().then(function(){
